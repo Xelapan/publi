c7e3804742990ab0c1d71be9000c63f90058eb0c
wichogg <wichogg@gmail.com>
2022-05-04 18:39:04 -0600

    Add files via upload

--- slideshw.py
+++ slideshw.py
@@ -5,16 +5,16 @@ import time
 from time import gmtime, strftime
 import glob, os
 
+cmd_chdir = ("cd " + os.getcwd())
 cmd_actualizar = "git pull https://github.com/wichogg/publi"
-cmd_reiniciar = "python test.py"
-global n
-#Cada cuantas images se reinicia
-n = 100
+cmd_reiniciar = "python slideshw.py"
+cmd_kill = "pkill slideshw.py"
 
 def verificarHorario():
   now = int(strftime("%H"))
   if now >= 22 and now <= 6:
     apagar = "echo 123 | sudo -S rtcwake -m mem --date " + strftime("%Y%m%d060100")
+    os.system(cmd_kill)
     os.system(apagar)
     os.system(cmd_reiniciar)
 
@@ -22,24 +22,19 @@ def slideShow():
   img = next(photos)
   displayCanvas.config(image=img)
   root.after(10000, slideShow) # xx seconds
-  n = n + 1
   #print("Sliding!! " + str(n))
-  # Verfica el horario cada 100 imagenes mostradas
-  if n > 100:
-    os.system(cmd_actualizar)
-    root.destroy()
 
 #### MAIN
+os.system(cmd_chdir)
+os.system(cmd_actualizar)
 root = tk.Tk()
 root.overrideredirect(True)
 root.config(cursor="none")  # hide the mouse cursor
 width = root.winfo_screenwidth()
 height = root.winfo_screenwidth()
-n = 0
-
 
 Album = []
-for image in glob.glob("*.png"):
+for image in glob.glob("*.jpg"):
     img = Image.open(image)
    # if img.width > width or img.height > height:
         # only resize image bigger than the screen
